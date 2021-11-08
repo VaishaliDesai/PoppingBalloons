b69e5f0a7c6799323a25b304e5403fe0d90a4644
VaishaliDesai <vaishali.desai@aexp.com>
2021-11-08 11:16:35 +0000

    First commit on setup

--- PoppingBalloons/GameViewController.swift
+++ PoppingBalloons/GameViewController.swift
@@ -1,49 +1,22 @@
-//
-//  GameViewController.swift
-//  PoppingBalloons
-//
-//  Created by Vaishali Desai on 08/11/2021.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let view = view as? SKView {
+      // Create the scene programmatically
+      let scene = GameScene(size: view.bounds.size)
+      scene.scaleMode = .resizeFill
+      view.ignoresSiblingOrder = true
+      view.showsFPS = true
+      view.showsNodeCount = true
+      view.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
 }
