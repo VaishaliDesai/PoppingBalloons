934b7b30b4808b883a98f769c420614e7c31cb7b
VaishaliDesai <vaishali.desai@aexp.com>
2021-11-10 10:14:25 +0000

    Added functions to fly random balloons in distributed manner

--- PoppingBalloons/GameScene.swift
+++ PoppingBalloons/GameScene.swift
@@ -1,88 +1,66 @@
-//
-//  GameScene.swift
-//  PoppingBalloons
-//
-//  Created by Vaishali Desai on 08/11/2021.
-//
-
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+  
+  override func didMove(to view: SKView) {
+    backgroundColor = .white
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    run(SKAction.repeatForever(
+      SKAction.sequence([
+        SKAction.run(flyBalloon),
+        SKAction.wait(forDuration: 1.0)
+      ])
+    ))
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+    let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
+    backgroundMusic.autoplayLooped = true
+    addChild(backgroundMusic)
+  }
+  
+  func flyBalloon() {
+    let balloon = SKSpriteNode(imageNamed: Color.allCases.randomElement()?.rawValue ?? Color.red.rawValue)
     
+    balloon.position = CGPoint(x: CGFloat.random(in: 0...size.width), y: 0) //random
+    addChild(balloon)
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
+    let actualDuration = CGFloat.random(in: 4.0...6.0)
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
+    ///fly balloons straight up
+//    let actualMove = SKAction.move(to: CGPoint(x: size.width/2, y: size.height), duration: TimeInterval(actualDuration))
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+    ///fly balloons more towards left
+    //let min = size.height/2
+//    let max = size.height - size.height/2
+    //let actualMove = SKAction.move(to: CGPoint(x: CGFloat.random(in: min...max), y: size.height), duration: TimeInterval(actualDuration))
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
+    ///fly balloons both the sides distributed
+    let max = size.width
+    let actualMove = SKAction.move(to: CGPoint(x: CGFloat.random(in: 0...max), y: size.height), duration: TimeInterval(actualDuration))
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
+    let actionMoveDone = SKAction.removeFromParent()
+    balloon.run(SKAction.sequence([actualMove, actionMoveDone]))
+  }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  func balloonBursts(balloon: SKNode) {
+    let balloonSpriteNode = SKSpriteNode(imageNamed: Color.allCases.randomElement()?.rawValue ?? Color.red.rawValue)
+    balloonSpriteNode.position = balloon.position
+    addChild(balloonSpriteNode)
+    balloon.removeFromParent()
+  }
 }
+
+public enum Color: String, CaseIterable {
+  case blue = "balloon_blue"
+  case brown = "balloon_brown"
+  case cyan = "balloon_cyan"
+  case green = "balloon_green"
+  case lime = "balloon_lime"
+  case olive = "balloon_olive"
+  case orange = "balloon_orange"
+  case pink = "balloon_pink"
+  case purple = "balloon_purple"
+  case red = "balloon_red"
+  case yellow = "balloon_yellow"
+}
+
+
--- PoppingBalloons/GameViewController.swift
+++ PoppingBalloons/GameViewController.swift
@@ -5,15 +5,14 @@ class GameViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    if let view = view as? SKView {
-      // Create the scene programmatically
-      let scene = GameScene(size: view.bounds.size)
-      scene.scaleMode = .resizeFill
-      view.ignoresSiblingOrder = true
-      view.showsFPS = true
-      view.showsNodeCount = true
-      view.presentScene(scene)
-    }
+
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    scene.scaleMode = .resizeFill
+    skView.presentScene(scene)
   }
   
   override var prefersStatusBarHidden: Bool {
