e9e30fc05c3ca5a31e1124e21566b2f8a238889f
VaishaliDesai <vaishali.desai@aexp.com>
2021-11-11 15:45:09 +0000

    Refactored game scene file

--- PoppingBalloons/GameScene.swift
+++ PoppingBalloons/GameScene.swift
@@ -25,11 +25,13 @@ class GameScene: SKScene {
   
   func addBackgroundWithMusic() {
     let background = SKSpriteNode(imageNamed: "background")
+    background.name = "background"
     background.position = CGPoint(x: size.width / 2, y: size.height / 2)
     background.scale(to: size)
     addChild(background)
     
     let backgroundMusic = SKAudioNode(fileNamed: "background.mp3")
+    backgroundMusic.name = "backgroundMusic"
     backgroundMusic.autoplayLooped = true
     addChild(backgroundMusic)
     
@@ -38,6 +40,7 @@ class GameScene: SKScene {
   
   func addScoreLabel() {
     scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+    scoreLabel.name = "scoreLabel"
     scoreLabel.fontColor = .red
     scoreLabel.fontSize = 20
     scoreLabel.text = "Score: 0"
@@ -47,16 +50,14 @@ class GameScene: SKScene {
   }
   
   func flyBalloon() {
-    balloon = SKSpriteNode(imageNamed: Color.allCases.randomElement()?.rawValue ?? Color.red.rawValue)
+    let xRange = CGFloat.random(in: 0...size.width)
+    balloon = SKSpriteNode(imageNamed: String(describing: Color.allCases.randomElement() ?? Color.red))
     
-    balloon.position = CGPoint(x: CGFloat.random(in: 0...size.width), y: 0) //random
-    balloon.xScale = 2
-    balloon.yScale = 2
+    balloon.position = CGPoint(x: xRange, y: 0) //random
+    balloon.setScale(2)
     balloon.physicsBody = SKPhysicsBody(circleOfRadius: balloon.size.width)
     balloon.physicsBody?.isDynamic = false
     addChild(balloon)
-    
-    let actualDuration = CGFloat.random(in: 4.0...6.0)
     
     ///fly balloons straight up
 //    let actualMove = SKAction.move(to: CGPoint(x: size.width/2, y: size.height), duration: TimeInterval(actualDuration))
@@ -67,8 +68,8 @@ class GameScene: SKScene {
     //let actualMove = SKAction.move(to: CGPoint(x: CGFloat.random(in: min...max), y: size.height), duration: TimeInterval(actualDuration))
     
     ///fly balloons both the sides distributed
-    let max = size.width
-    let actualMove = SKAction.move(to: CGPoint(x: CGFloat.random(in: 0...max), y: size.height), duration: TimeInterval(actualDuration))
+    let speed = TimeInterval(CGFloat.random(in: 4.0...6.0))
+    let actualMove = SKAction.move(to: CGPoint(x: xRange, y: size.height), duration: speed)
     
     let actionMoveDone = SKAction.removeFromParent()
     balloon.run(SKAction.sequence([actualMove, actionMoveDone]))
@@ -79,12 +80,13 @@ class GameScene: SKScene {
     balloon.removeFromParent()
     score += 1
     
-    addCelebration(ballon: balloon)
+    addCelebration(balloon: balloon)
   }
   
-  func addCelebration(ballon: SKNode) {
+  func addCelebration(balloon: SKNode) {
     let size = size.width * 0.01
     let emitter = SKEmitterNode()
+    emitter.name = "emitter"
     emitter.particleSize = CGSize(width: size, height: size)
     emitter.particleZPosition = 2
     emitter.numParticlesToEmit = 100
@@ -93,7 +95,7 @@ class GameScene: SKScene {
     emitter.emissionAngleRange = 360 * .pi / 180
     emitter.particleSpeed = 700
     emitter.particleColor = .red
-    emitter.position = ballon.position
+    emitter.position = balloon.position
     emitter.particleColorSequence = SKKeyframeSequence(
       keyframeValues: [SKColor.yellow, SKColor.red, SKColor.lightGray,SKColor.gray],
       times: [0, 0.25, 0.5, 1]
@@ -114,18 +116,18 @@ class GameScene: SKScene {
   }
 }
 
-public enum Color: String, CaseIterable {
-  case blue = "blue"
-  case brown = "brown"
-  case cyan = "cyan"
-  case green = "green"
-  case lime = "lime"
-  case olive = "olive"
-  case orange = "orange"
-  case pink = "pink"
-  case purple = "purple"
-  case red = "red"
-  case yellow = "yellow"
+public enum Color: CaseIterable {
+  case blue
+  case brown
+  case cyan
+  case green
+  case lime
+  case olive
+  case orange
+  case pink
+  case purple
+  case red
+  case yellow
 }
 
 
